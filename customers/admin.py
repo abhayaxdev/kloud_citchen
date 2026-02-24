091196dc9180d7204a2b0b61c3d4c7fe0411a022
abhaya <gautamabhaya98@gmail.com>
2026-02-24 22:43:33 +0545

    feat: customer model created

--- customers/admin.py
+++ customers/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Customer
 
-# Register your models here.
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('email', 'contact_number', 'created_at', 'updated_at')
+    list_filter = ('created_at', 'email')
+    search_fields = ('first_name', 'last_name', 'email', 'contact_number')
+    
--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,61 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+class BaseActivityModel(models.Model):
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
+    
+    class Meta:
+        abstract = True
+        
+        
+        
+        
+class Customer(BaseActivityModel):
+    """ A basic customer model to store all necessary customer information. """
+    user = models.OneToOneField(
+        User, 
+        on_delete=models.CASCADE, 
+        related_name="customer"
+    )
+    first_name = models.CharField(
+        max_length=50, 
+        verbose_name="First Name"    
+    )
+    last_name = models.CharField(
+        max_length=50, 
+        verbose_name="Last Name"    
+    )
+    username = models.CharField(
+        max_length=50,
+        null=True,
+        blank=True
+    )
+    email = models.EmailField(
+        max_length=255
+    )
+    contact_number = models.CharField(
+        max_length=15
+    )
+    # address = models.
+    
+    
+    class Meta:
+        verbose_name = "Customer"
+        verbose_name_plural = "Customers"
+    
+    
+    def save(self, *args, **kwargs):
+        if not self.username:
+            self.username = self.email
+        super().save(*args, **kwargs)    
+    
+    
+    def __str__(self):
+        return self.username
